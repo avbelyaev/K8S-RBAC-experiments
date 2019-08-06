a8fce0118425ec314b28411d36567a690ec3c824
Anton Belyaev <anton.belyaev@bostongene.com>
2019-08-06 14:58:08 +0300

    serializing responses to json. added authn to DB

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3.6
+import json
 import os
 from flask import Flask
 from flask_mongoengine import MongoEngine, Document
@@ -9,8 +10,8 @@ from functools import partial
 print = partial(print, flush=True)
 
 
-MONGO_HOST = os.environ['MONGO_HOST']
-MONGO_PORT = int(os.environ['MONGO_PORT'])
+MONGO_HOST = os.environ.get('MONGO_HOST') if os.environ.get('MONGO_HOST') is not None else 'localhost'
+MONGO_PORT = 27017
 
 
 class User(Document):
@@ -19,9 +20,11 @@ class User(Document):
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'users',
+    'db': 'kube',
     'host': MONGO_HOST,
-    'port': MONGO_PORT
+    'port': MONGO_PORT,
+    'username': 'admin',
+    'password': 'admin'
 }
 db = MongoEngine()
 
@@ -30,35 +33,47 @@ db = MongoEngine()
 def save_user(email):
     print(f'Attempting to save user with email {email}')
 
-    existing_user = next(filter(lambda user_doc: user_doc.email == email, User.objects), None)
+    users = User.objects
+    existing_user = next(filter(lambda user_doc: user_doc.email == email, users), None)
     if existing_user:
         print(f'User with email {email} already exists')
-        return 'Ignored as duplicate\n'
+        return app.response_class(
+            response=json.dumps({'msg': 'user already exists'}),
+            status=409,
+            mimetype='application/json'
+        )
 
     else:
         user = User()
         user.email = email
         user.save()
         print(f'User with email {email} has been successfully saved')
-        return f'User {email} has been saved!\n'
+        return app.response_class(
+            response=json.dumps({'msg': f'User {email} has been saved!\n'}),
+            status=200,
+            mimetype='application/json'
+        )
 
 
 @app.route("/users")
 def list_users():
     print('Listing users')
-
     users = User.objects
-    if users:
-        return '\n'.join(map(lambda user: user.email, users))
-
-    else:
-        return 'User list is empty\n'
+    return app.response_class(
+        response=json.dumps(users),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 @app.route("/headers")
 def headers():
     rq_headers = request.headers
-    return f'Headers:{rq_headers}'
+    return app.response_class(
+        response=json.dumps(rq_headers),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 @app.route("/hello")
@@ -69,4 +84,4 @@ def hello():
 if __name__ == '__main__':
     print(f'Connecting to mongo at {MONGO_HOST}:{MONGO_PORT}')
     db.init_app(app)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')     # to be able to run in container
