0fffa4206ef5b9a4450b6eb76f7fc7d8a691d835
Anton Belyaev <anton.belyaev@bostongene.com>
2019-08-06 15:57:40 +0300

    replcaed flask_mongoengine with PyMongo

--- app.py
+++ app.py
@@ -1,66 +1,71 @@
 #! /usr/bin/env python3.6
-import json
 import os
 from flask import Flask
-from flask_mongoengine import MongoEngine, Document
 from flask import request
-from mongoengine import StringField
+from flask_pymongo import PyMongo
 from functools import partial
+from bson.json_util import dumps
 
 print = partial(print, flush=True)
 
-
 MONGO_HOST = os.environ.get('MONGO_HOST') if os.environ.get('MONGO_HOST') is not None else 'localhost'
 MONGO_PORT = 27017
-
-
-class User(Document):
-    email = StringField(required=True)
-
+MONGO_DB = 'kube'
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'kube',
-    'host': MONGO_HOST,
-    'port': MONGO_PORT,
-    'username': 'admin',
-    'password': 'admin'
-}
-db = MongoEngine()
+app.config['MONGO_URI'] = f'mongodb://admin:admin@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}'
+mongo = PyMongo(app)
+
+#
+# @app.route('/users/<email>', methods=['POST'])
+# def save_user(email):
+#     print(f'Attempting to save user with email {email}')
+#
+#     users = User.objects
+#     existing_user = next(filter(lambda user_doc: user_doc.email == email, users), None)
+#     if existing_user:
+#         print(f'User with email {email} already exists')
+#         return app.response_class(
+#             response=json.dumps({'msg': 'user already exists'}),
+#             status=409,
+#             mimetype='application/json'
+#         )
+#
+#     else:
+#         user = User()
+#         user.email = email
+#         user.save()
+#         print(f'User with email {email} has been successfully saved')
+#         return app.response_class(
+#             response=json.dumps({'msg': f'User {email} has been saved!\n'}),
+#             status=200,
+#             mimetype='application/json'
+#         )
 
 
-@app.route('/users/<email>', methods=['POST'])
-def save_user(email):
-    print(f'Attempting to save user with email {email}')
+@app.route('/any', methods=['POST'])
+def save_any():
+    data = request.get_json()
+    print(f'Saving {data}')
 
-    users = User.objects
-    existing_user = next(filter(lambda user_doc: user_doc.email == email, users), None)
-    if existing_user:
-        print(f'User with email {email} already exists')
-        return app.response_class(
-            response=json.dumps({'msg': 'user already exists'}),
-            status=409,
-            mimetype='application/json'
-        )
-
-    else:
-        user = User()
-        user.email = email
-        user.save()
-        print(f'User with email {email} has been successfully saved')
-        return app.response_class(
-            response=json.dumps({'msg': f'User {email} has been saved!\n'}),
-            status=200,
-            mimetype='application/json'
-        )
-
-
-@app.route("/users")
-def list_users():
-    print('Listing users')
-    users = User.objects
+    mongo.db.kube.insert_one(data)
     return app.response_class(
-        response=json.dumps(users),
+        response=dumps({'msg': f'request body has been saved!\n'}),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route("/all", methods=['GET'])
+def get_all():
+    print('Listing data')
+    objects = []
+    items = mongo.db.kube.find({})
+    for item in items:
+        objects.append(item)
+
+    return app.response_class(
+        response=dumps({'items': objects}),
         status=200,
         mimetype='application/json'
     )
@@ -69,11 +74,7 @@ def list_users():
 @app.route("/headers")
 def headers():
     rq_headers = request.headers
-    return app.response_class(
-        response=json.dumps(rq_headers),
-        status=200,
-        mimetype='application/json'
-    )
+    return f'Headers from rq:\n{rq_headers}'
 
 
 @app.route("/hello")
@@ -83,5 +84,4 @@ def hello():
 
 if __name__ == '__main__':
     print(f'Connecting to mongo at {MONGO_HOST}:{MONGO_PORT}')
-    db.init_app(app)
-    app.run(host='0.0.0.0')     # to be able to run in container
+    app.run(host='0.0.0.0', port=5000)  # to be able to run in container
