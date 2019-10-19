7928a73240e7084c2aaa866256c4088ac34248c5
anthony <antonsdl@gmail.com>
2019-10-20 01:56:14 +0300

    finally added pytest tests

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,6 +1,3 @@
-from unittest import mock
-from unittest.mock import Mock
-
 import pytest
 
 from backend.app import get_app_for_test
@@ -14,15 +11,33 @@ def app():
 
 def test_hello_flask(client):
     response = client.get("/api/hello")
+
     assert response.status_code == 200
-    assert response.json['msg'] == 'Hello flask!'
+    assert response.json['info'] == 'Hello flask!'
 
 
-@mock.patch('backend.mongo')
-def test_should_return_list_of_items(client, mock):
-    mock_methods = {'fuck.return_value': 3}
-    mongo_mock = Mock(**mock_methods)
-    print(mongo_mock.fuck())
-    mocker.patch('backend.mongo', return_value=mongo_mock)
+def test_should_return_empty_list_of_items(client, mocker):
+    mocker.patch('backend.app.DataStore.find_all', return_value=[])
     response = client.get("/api/docs")
-    assert response.json['items'] == None
+
+    assert response.status_code == 200
+    assert response.json['items'] == []
+
+
+def test_should_return_list_of_items(client, mocker):
+    expected_items = [
+        {
+            'firstname': 'tony',
+            'lastname': 'stark'
+        },
+        {
+            'firstname': 'steve',
+            'lastname': 'rogers'
+        }
+    ]
+    # mock DataStore inside the scope of backend.app module
+    mocker.patch('backend.app.DataStore.find_all', return_value=expected_items)
+    response = client.get("/api/docs")
+
+    assert response.status_code == 200
+    assert response.json['items'] == expected_items
