4419d510ecc08525104c56714cdf14696bea585e
anthony <antonsdl@gmail.com>
2019-11-11 00:13:02 +0300

    fixed test

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -13,7 +13,7 @@ def test_hello_flask(client):
     response = client.get("/api/hello")
 
     assert response.status_code == 200
-    assert response.json['info'] == 'Hello flask!'
+    assert response.json['info'] == 'Hello flask 2!'
 
 
 def test_should_return_empty_list_of_items(client, mocker):
